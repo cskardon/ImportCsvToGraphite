a62db0aaa179abee5745efd423402f3965ecc27a
Chris Skardon <chris@tournr.com>
2017-08-23 11:08:30 +0100

    Code addition

---
+++ ImportCsvToGraphite/ImportCsvToGraphite/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace ImportCsvToGraphite
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Sockets;
+
+    internal class Program
+    {
+        private static string _hostname = "localhost";
+        private static string _directory = "c:\\temp\\metrics";
+        private static int _port = 2003;
+        private static int _rebaseSeconds;
+        private static string _prefix = "Neo4j";
+        private static int _incrementRebaseSeconds = 5;
+
+        private static bool AreParametersOk(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i].ToLowerInvariant())
+                {
+                    case "-hostname":
+                    case "-host":
+                        _hostname = args[++i];
+                        break;
+                    case "-port":
+                        if (int.TryParse(args[++i], out int port))
+                            _port = port;
+                        else throw new ArgumentOutOfRangeException("port", args[i], "Port isn't a number!");
+                        break;
+                    case "-directory":
+                    case "-d":
+                        _directory = args[++i];
+                        break;
+                    case "-rebaseseconds":
+                    case "-rs":
+                        if (int.TryParse(args[++i], out int rebaseSeconds)) _rebaseSeconds = rebaseSeconds;
+                        else throw new ArgumentOutOfRangeException("rebaseSeconds", args[i], "Seconds provided for RebaseSeconds wasn't a number");
+                        break;
+                    case "-rebasesecondsincrement":
+                    case "-rsi":
+                        if (int.TryParse(args[++i], out int rebaseIncrement)) _incrementRebaseSeconds = rebaseIncrement;
+                        else throw new ArgumentOutOfRangeException("rebaseIncrementSeconds", args[i], "Seconds provided to increment RebaseSeconds wasn't a number.");
+                        break;
+                    case "-prefix":
+                        _prefix = args[++i];
+                        break;
+                    default:
+                        Console.WriteLine($"Unknown parameter supplied: '{args[i]}'");
+                        ShowUsageMessage();
+                        return false;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(_hostname) || string.IsNullOrWhiteSpace(_prefix) || string.IsNullOrWhiteSpace(_directory))
+                return false;
+
+            return true;
+        }
+
+        private static void Main(string[] args)
+        {
+            if (!AreParametersOk(args))
+            {
+                ShowUsageMessage();
+                return;
+            }
+
+            var client = new TcpClient();
+            client.Connect(_hostname, _port);
+            Console.WriteLine($"Connected to the Graphite end point ({client.Connected}).");
+
+            var stream = new StreamWriter(client.GetStream());
+
+            var files = Directory.GetFiles(_directory, "*.csv");
+            foreach (var file in files)
+            {
+                var fInfo = new FileInfo(file);
+
+                var secondsFromEpoc = SecondsSinceEpoc(TimeSpan.FromSeconds(_rebaseSeconds));
+
+                var stat = fInfo.Name.Replace(".csv", "");
+                var lines = File.ReadAllLines(file).Skip(1);
+                foreach (var line in lines)
+                {
+                    var split = line.Split(",".ToCharArray());
+
+                    var newLine = _rebaseSeconds != 0
+                        ? $"{_prefix}.{stat} {split[1]} {secondsFromEpoc}"
+                        : $"{_prefix}.{stat} {split[1]} {split[0]} ";
+
+                    if (_rebaseSeconds != 0)
+                        secondsFromEpoc += _incrementRebaseSeconds;
+
+                    Console.WriteLine($"Writing {newLine} to Stream (which is: {new DateTime(1970, 1, 1).AddSeconds(secondsFromEpoc):yyyy-MM-dd HH:mm:ss})");
+                    stream.WriteLine($"{newLine}");
+                }
+            }
+
+            stream.Dispose();
+            client.Dispose();
+
+            Console.WriteLine("In theory they're all there!");
+            Console.ReadLine();
+        }
+
+        private static void ShowUsageMessage()
+        {
+            Console.WriteLine("dotnet run -host localhost -port 2003 -rs 3600 -d c:\\temp\\metrics");
+        }
+
+
+        private static int SecondsSinceEpoc(TimeSpan? hoursToSubtract = null)
+        {
+            if (hoursToSubtract == null)
+                hoursToSubtract = TimeSpan.Zero;
+
+            var t = (DateTime.Now - new DateTime(1970, 1, 1)).Subtract(hoursToSubtract.Value);
+            return (int)t.TotalSeconds;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# ImportCsvToGraphite
-Imports CSV files to Graphite
+# Import Csv To Graphite
+
+The clue is in the title, this imports CSV files to Graphite.
+
+### Why?
+
+Graphite has a 'PlainText' 
+
+### What does this work on?
+
+It's a .NET Core application, so it should work on a Mac, Linux or Windows - I've personally tested on Windows and Linux with no problems
+
+### OK Great, let's do this
+
+1. You need .NET Core which you can get from [here](https://www.microsoft.com/net/core).
+   
+   1. Install that bad boy
+2. Download the source
+3. Navigate to the directory (in a console/terminal window)
+4. Run `dotnet build`
+5. Run it! (see the parameters section if you don't have a default Graphite install)
+
+   1. Windows: `dotnet run -d c:\temp\metrics`
+   2. Linux: `dotnet run -d /mnt/c/temp/metrics` 
+
+### Parameters
+
+- [Required] `-d/-directory <DIRECTORY>` the directory the metrics are in
+- `-host <HOSTNAME>` - the host the Graphite instance is running on (default: `localhost`)
+- `-port <PORT>` - the port for the Graphite PlainText Carbon protocol (default: `2003`)
+- `-rs/-rebaseSeconds` - the number of seconds to rebase the log files to, i.e. it will restart all the log entries at `today - rebaseSeconds` (default: `0`)
+  
+  - If this is `0` then the importer will use the timestamp in the csv files
+- `-rsi/-rebaseSecondsIncrement` - the number of seconds to increment for each line in the csv files (default: `5`).
+- `-prefix` - the prefix to put in front of the metrics (default: `Neo4j`)
+
+### Works with
+
+* https://github.com/igorborojevic/grafana_neo4j
+
+### Todos
+
+- Better 'Usage' instructions
